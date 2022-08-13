ec2a255e0b3c36ddbd9baa2ea8c8c484b78b276b
WiaanB <whoppingwaki@gmail.com>
2022-08-14 00:05:29 +0200

    added the user type

---
+++ src/data/Types.tsx
@@ -0,0 +1,14 @@
+export interface User {
+    id: number,
+    username: string,
+    name: string,
+    surname: string,
+    age: number,
+    employed: boolean,
+    job: {
+        title: string,
+        company: string,
+        period: number
+    },
+    hobbies: string[]
+}
