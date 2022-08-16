e89caf550e1e5754cc25caac310ddcd2d8c492df
WiaanB <whoppingwaki@gmail.com>
2022-08-16 21:32:12 +0200

    basic grid settings added

---
+++ src/data/GridSettings.js
@@ -0,0 +1,10 @@
+module.exports = {
+    columns: [
+        { field: "id", type: "number", order: 0 },
+        { field: "username", type: "string", order: 1 },
+        { field: "employed", type: "boolean", order: 2 },
+        { field: "hobbies", type: "array", order: 3 },
+        { field: "job.title", title: "job", type: "string", order: 4 }
+    ],
+    identifying_field: "id"
+}
