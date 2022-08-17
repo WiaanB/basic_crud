9462a2ef1dbc7c1b480d692477ea73c806b18c39
WiaanB <whoppingwaki@gmail.com>
2022-08-17 21:18:19 +0200

    added a hidden property

--- src/data/GridSettings.js
+++ src/data/GridSettings.js
@@ -4,7 +4,7 @@ module.exports = {
         { field: "username", type: "string", order: 1 },
         { field: "employed", type: "boolean", order: 2 },
         { field: "hobbies", type: "array", order: 3 },
-        { field: "job.title", title: "job", type: "string", order: 4 }
+        { field: "job.title", title: "job", type: "string", hidden: false, order: 4 }
     ],
     identifying_field: "id"
 }
