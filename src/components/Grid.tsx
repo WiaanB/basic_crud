86743c56b41ee6c304986e0235b8671de591dea3
WiaanB <whoppingwaki@gmail.com>
2022-08-17 21:18:41 +0200

    started the rendering of the grid

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,10 +1,39 @@
 import { User } from '../data/Types'
+import grid_settings from '../data/GridSettings'
+import { useEffect, useState } from 'react'
 
 function Grid(props: {Users: User[]}) {
-    console.log({props})
+    const [columns,setColumns] = useState<any>([])
+
+    // setup of the basics of the grid
+    useEffect(() => {
+        setupGridColumns();
+    },[])
+
+    function setupGridColumns() {
+        let cols = grid_settings.columns ?? [];
+        cols.sort((a,b) => Number(a) - Number(b) ).map(col => {
+            let formattedCol:any = {
+                col_field: col.field,
+                col_type: col.type
+            }
+            formattedCol.col_title = col.title ? col.title:col.field
+            formattedCol.col_hidden = col.hidden ? col.hidden:false
+            return formattedCol
+        })
+        console.log({cols})
+        setColumns(cols);
+    }
     return (
-        <div className="App">
-            <h1>Hello World</h1>
+        <div className="grid-main">
+            <div className="grid-container">
+                <div className="grid-columns" style={{ gridTemplateColumns: `repeat(${columns.filter((e:any) => e.hidden === false).length})` }}>
+                    {columns.length && columns.map((col:any) => {
+                        console.log(col)
+                        return <li>{col.col_title}</li>
+                    })}
+                </div>
+            </div>
         </div>
     );
 }
