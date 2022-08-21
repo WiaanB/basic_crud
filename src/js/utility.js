5b0ecc28461bf535eb02f308993158319ce0446d
WiaanB <whoppingwaki@gmail.com>
2022-08-21 18:28:17 +0200

    function to rebuild objects into correct format after editing

--- src/js/utility.js
+++ src/js/utility.js
@@ -17,3 +17,27 @@ export const flattenObject = (object, parent) => {
     })
     return flatObject;
 }
+
+export const rebuildObject = (object) => {
+     // new object to be returned
+    let rebuiltObject = {}
+    // go over each of the fields in the object
+    Object.keys(object).forEach(key => {
+        // check if it is indeed a flattened object or not
+        if (key.includes('.')) {
+            // splitKey is to find the original object's name
+            let splitKey = key.split('.')
+            // rejoinedKey is for the remainder of the field name
+            let rejoinedKey = splitKey.slice(1).join('.')
+            // if it needs to be built up further, i.e another nested object, continue the loop
+            if (rejoinedKey.includes('.')) {
+                // spread out the object so that the existing fields remain, as well as adding the new rebuilt object (of which there might already be existing fields as well)
+                rebuiltObject[splitKey[0]] = { ...rebuiltObject[splitKey[0]], ...rebuildObject({ ...rebuiltObject[splitKey[0]], [rejoinedKey]: object[key] }) }
+            }
+            // spread out the object so that the existing fields remai
+            else rebuiltObject[splitKey[0]] = { ...rebuiltObject[splitKey[0]], [rejoinedKey]: object[key] }
+        }
+        else rebuiltObject[key] = object[key]
+    })
+    return rebuiltObject
+}
